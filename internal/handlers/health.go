5c97e3f577e3098eb6faa38ebeebb1929641e4cc
Henry Bugajski <henry.bug.dev@gmail.com>
2025-03-12 18:43:49 -0400

    fix: check return value

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -1,9 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"video-optimization/internal/logger"
+)
 
 func Health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK\n"))
+	if _, err := w.Write([]byte("OK\n")); err != nil {
+		logger.Logger.Errorw("Failed to write response", "error", err)
+	}
 }
